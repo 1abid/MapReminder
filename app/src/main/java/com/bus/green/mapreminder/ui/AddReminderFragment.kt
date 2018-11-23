7e9058a5f74d445363e9780f2d6e27aabf7b5f24
Abid Hasan <la4508@example.com>
2018-11-23 21:01:26 +0600

    add bootsheet on add reminder fragment WIP

--- app/src/main/java/com/bus/green/mapreminder/ui/AddReminderFragment.kt
+++ app/src/main/java/com/bus/green/mapreminder/ui/AddReminderFragment.kt
@@ -9,8 +9,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.airbnb.lottie.LottieAnimationView
 import com.bus.green.mapreminder.R
 import com.bus.green.mapreminder.common.addMapStyle
+import com.bus.green.mapreminder.common.animateCamera
 import com.bus.green.mapreminder.common.hideKeyboard
 import com.bus.green.mapreminder.common.setCameraPosition
 import com.bus.green.mapreminder.location.LocationProvider
@@ -20,6 +22,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_add_reminder.*
+import kotlinx.android.synthetic.main.fragment_main.*
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -60,6 +63,8 @@ class AddReminderFragment : Fragment(), OnMapReadyCallback {
             if(!isChecked){
                 search_place.setText("", TextView.BufferType.EDITABLE)
                 context?.hideKeyboard(buttonView)
+                if(map_marker.visibility == View.GONE)
+                    map_marker.markerVisibility()
             }
 
         }
@@ -84,6 +89,9 @@ class AddReminderFragment : Fragment(), OnMapReadyCallback {
             this?.uiSettings?.isMapToolbarEnabled = false
             this?.isMyLocationEnabled = true
         }
+        user_location?.setOnClickListener {
+            map?.animateCamera(latLng)
+        }
     }
 
 
@@ -99,15 +107,19 @@ class AddReminderFragment : Fragment(), OnMapReadyCallback {
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             if(count > 0){
                 search_iv?.apply { if(!this.isChecked) this.isChecked = true }
+                map_marker.markerVisibility(View.GONE)
             }
         }
 
         override fun afterTextChanged(s: Editable?) {
             if(s?.length!! == 0){
                 search_iv?.apply { if(this.isChecked) this.isChecked = false }
+                map_marker.markerVisibility()
             }
         }
 
     }
+
+    private fun LottieAnimationView.markerVisibility(visibility: Int = View.VISIBLE) { this.visibility = visibility }
 }
 
