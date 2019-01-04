072d3c41cd33f79989ba20a5410dfbc4c1cc9d9a
Abid Hasan <la4508@example.com>
2019-01-04 21:24:35 +0600

    current locatio live data tested

--- app/src/test/java/com/bus/green/mapreminder/model/CurrentLocationLiveDataTest.kt
+++ app/src/test/java/com/bus/green/mapreminder/model/CurrentLocationLiveDataTest.kt
@@ -1,14 +1,17 @@
 package com.bus.green.mapreminder.model
 
 
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.Observer
 import com.bus.green.mapreminder.InstantTaskExecutorExtension
 import com.bus.green.mapreminder.location.LocationProvider
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.whenever
+import com.nhaarman.mockito_kotlin.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
 
@@ -28,6 +31,92 @@ class CurrentLocationLiveDataTest {
     fun setUp(){
         whenever(lifeCycleOwner.lifecycle).thenReturn(lifecycleRegistry)
         currentLocationLiveData.observe(lifeCycleOwner, observer)
+    }
+
+    @Nested
+    @DisplayName("Given a CurrentLocationLiveData which is not active")
+    inner class InActive{
+
+        @BeforeEach
+        fun setUp(){
+            lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+        }
+
+        @Nested
+        @DisplayName("When the location is updated")
+        inner class NeverActive{
+
+            @BeforeEach
+            fun  setUp(){
+                testLocationProvider.trigger()
+            }
+
+            @Test
+            @DisplayName("We never receive a location callback")
+            fun noCallBack(){
+                verify(observer, never()).onChanged(any())
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("Given a CurrentLocationLiveData which is active")
+    inner class Active{
+
+        @BeforeEach
+        fun setUp(){
+            lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
+        }
+
+
+        @Nested
+        @DisplayName("When the location is updated")
+        inner class Activated{
+            @BeforeEach
+            fun setUp(){
+                testLocationProvider.trigger()
+            }
+
+            @Test
+            @DisplayName("then we receive a location update")
+            fun receiveACallBack(){
+                verify(observer, times(1)).onChanged(any())
+            }
+        }
+    }
+
+
+    @Nested
+    @DisplayName("Given a CurrentLocationLiveData which was active")
+    inner class WasActive{
+
+        @BeforeEach
+        fun setUp(){
+            lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
+            lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
+        }
+
+
+        @Nested
+        @DisplayName("When the location is updated")
+        inner class Activated{
+            @BeforeEach
+            fun setUp(){
+                testLocationProvider.trigger()
+            }
+
+            @Test
+            @DisplayName("then we receive no location update")
+            fun noCallBack(){
+                verify(observer, never()).onChanged(any())
+            }
+        }
+
+
     }
 
 
